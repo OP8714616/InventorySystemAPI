e07f60c2dc7dece2866e0983f4a5c0d31dbb540f
op8714616 <tpps8611@gmail.com>
2025-10-28 15:31:00 +0800

    Initial commit - 完成商品管理 API

---
+++ InventorySystemAPI/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using InventorySystemAPI.Models;
+
+namespace InventorySystemAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        // 商品資料表
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ InventorySystemAPI/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace InventorySystemAPI.Models
+{
+    public class Product
+    {
+        // 商品編號(主鍵)
+        public int Id { get; set; }
+
+        // 商品名稱
+        public string Name { get; set; } = string.Empty;
+
+        // 商品描述
+        public string? Description { get; set; }
+
+        // 商品價格
+        public decimal Price { get; set; }
+
+        // 庫存數量
+        public int Stock { get; set; }
+
+        // 建立時間
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
